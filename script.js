c10f26f85d177a4b32b2cdb0ac1831a268a48d0a
recanman <29310982+recanman@users.noreply.github.com>
2024-08-30 00:13:55 -0700

    progress

--- script.js
+++ script.js
@@ -23,5 +23,51 @@ function initializeGraph() {
         { from: 1, to: 3, weight: 30 }
     ];
     
-    //drawGraph();
+    drawGraph();
 }
+
+function drawGraph() {
+    graphContainer.innerHTML = '';
+
+    nodes.forEach(node => {
+        const nodeElement = document.createElement('div');
+        nodeElement.className = 'node';
+        nodeElement.style.left = `${node.x}px`;
+        nodeElement.style.top = `${node.y}px`;
+        nodeElement.innerText = node.id;
+        nodeElement.dataset.id = node.id;
+        graphContainer.appendChild(nodeElement);
+    });
+
+    edges.forEach(edge => {
+        const fromNode = nodes.find(node => node.id === edge.from);
+        const toNode = nodes.find(node => node.id === edge.to);
+        const edgeElement = document.createElement('div');
+        edgeElement.className = 'edge';
+        const length = Math.sqrt(Math.pow(toNode.x - fromNode.x, 2) + Math.pow(toNode.y - fromNode.y, 2));
+        edgeElement.style.width = `${length}px`;
+        edgeElement.style.transformOrigin = '0 0';
+        edgeElement.style.transform = `rotate(${Math.atan2(toNode.y - fromNode.y, toNode.x - fromNode.x) * 180 / Math.PI}deg)`;
+        edgeElement.style.left = `${fromNode.x}px`;
+        edgeElement.style.top = `${fromNode.y}px`;
+        graphContainer.appendChild(edgeElement);
+    });
+}
+
+function dijkstra(start, end) {
+    //todo this one will take time
+    const startNode = document.querySelector(`.node[data-id="${start}"]`);
+    const endNode = document.querySelector(`.node[data-id="${end}"]`);
+    startNode.classList.add('start-node');
+    endNode.classList.add('end-node');
+}
+
+runAlgorithmButton.addEventListener('click', () => {
+    const start = parseInt(startNodeInput.value, 10);
+    const end = parseInt(endNodeInput.value, 10);
+    if (!isNaN(start) && !isNaN(end)) {
+        dijkstra(start, end);
+    }
+});
+
+initializeGraph();
